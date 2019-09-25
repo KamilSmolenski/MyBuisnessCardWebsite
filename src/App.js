1dbffeed96a239a6cfa2849ff689b85c3c4af9de
kamilsmolen.ski <smolesnkikamil@gmail.com>
2019-09-25 21:28:12 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { BrowserRouter, Link, Route } from 'react-router-dom'
+const Home = () => <h1>Strona Startowa</h1>
+const News = () => <h1>Aktualnosci</h1>
+const Contact = () => <h1>Kontakt</h1>
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Start</Link>
+              </li>
+              <li>
+                <Link to='/news'>Aktualnosci</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Kontakt</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <Route path='/' exact component={Home} />
+          <Route path='/news' component={News} />
+          <Route path='/contact' component={Contact} />
+        </section>
+      </div>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
